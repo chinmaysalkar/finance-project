a65c42f0f30929370cca5ce4bfcb7cc5ff869d52
chinmaysalkar <120375162+chinmaysalkar@users.noreply.github.com>
2023-06-28 15:30:02 +0530

    Update main.tf

--- prod-server/main.tf
+++ prod-server/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "prod-server" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
-  key_name               = "keypairpem"
-  vpc_security_group_ids = ["sg-0a5b8d6ca31ae2d81"]
+  key_name               = "myprojetct01"
+  vpc_security_group_ids = ["sg-00de37b3b55eb6077"]
   tags = {
-    Name = "test-server"
+    Name = "prod-server"
   }
   
   provisioner "local-exec" {
@@ -14,16 +14,16 @@ resource "aws_instance" "test-server" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("./keypairpem.pem")
+    private_key = file("./mtprojetct01.pem")
     host        = self.public_ip 
   }
    
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+    command = "echo ${aws_instance.prod-server.public_ip} > inventory"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/finance-project/test-server/bankdeployplaybook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/finance-project/prod-server/deploy.yml"
   }
 }
 
